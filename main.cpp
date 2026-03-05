9388043d080bb879777f7698924e228579a044fc
RapidMindd <YaroslavT007@yandex.ru>
2026-03-05 22:46:48 +0300

    description added

--- main.cpp
+++ main.cpp
@@ -1,3 +1,17 @@
+/*
+Неявный интерфейс:
+1. Конструктор по умолчанию
+2. Перегруженный оператор меньше
+3. Перегруженный оператор копирующего присваивания
+4. Деструктор
+
+Недостатки явного интерфейса:
+1. Память выделяется вне функции. Если пользователь выделил недостаточно памяти, то функция небезопасна.
+2. Если в процессе заполнения массива генерируется исключение, то пользователь не может узнать,
+   сколько элементов успело скопироваться в c (базовая гарантия)
+3. По сигнатуре сложно понять, что нужно передать в функцию и какой ожидается результат
+*/
+
 #include <iostream>
 
 template< class T>
@@ -14,25 +28,25 @@ T* mergeN(const T* const* a, size_t sa, const size_t* sai, T* c)
   {
     for (size_t k = 0; k < size; ++k)
     {
-      T min;
+      T min; // T::T()
       bool isFirstElem = true;
       size_t index_temp = 0;
       for (size_t i = 0; i < sa; ++i)
       {
         if (indices[i] < sai[i])
         {
-          if (isFirstElem || a[i][indices[i]] < min)
+          if (isFirstElem || a[i][indices[i]] < min) // T::operator<(const T &)
           {
-            min = a[i][indices[i]];
+            min = a[i][indices[i]]; //T::operator=(const T &)
             index_temp = i;
             isFirstElem = false;
           }
         }
       }
-      c[index_c] = min;
+      c[index_c] = min; //T::operator=(const T &)
       index_c++;
       indices[index_temp]++;
-    }
+    } // T::~T() для min
   }
   catch (...)
   {
